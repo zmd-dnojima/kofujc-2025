9fecabedd5e2e0fa83936fd8d57c431ef9ef9b98
zmd <dnojima@zmd.jp>
2024-11-21 16:01:58 +0900

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@ require('dotenv').config({path: `./.env.${env}`});
 
 module.exports = {
   siteMetadata: {
-    defaultTitle: `【公式】株式会社 柳川芳鉄工所`,
-    defaultDescription: `【公式】株式会社 柳川芳鉄工所`,
+    defaultTitle: `一般社団法人甲府青年会議所 2025年度 - Here We Go! ～その一歩で君が変わる、まちが変わる～`,
+    defaultDescription: `一般社団法人甲府青年会議所は「修練」「奉仕」「友情」の３信条のもと、「明るい豊かな社会の実現」を目指し「まちづくり」「ひとづくり」「組織づくり」をしている団体です。`,
     defaultImage: "src/image/social-card.png",
     siteUrl: "https://yanagawa-yoshi.co.jp",
   },
@@ -25,14 +25,21 @@ module.exports = {
         plugins: [`gatsby-remark-autolink-headers`],
       },
     },
-    {
-      resolve: `gatsby-plugin-robots-txt`,
-      options: {
-        host: `https://yanagawa-yoshi.co.jp`,  //変更
-        sitemap: `https://yanagawa-yoshi.co.jp/sitemap-index.xml`, //変更
-        policy: [{ userAgent: `*`, allow: `/` }]
-      }
-    },
+    // {
+    //   resolve: `gatsby-plugin-robots-txt`,
+    //   options: {
+    //     host: `https://yanagawa-yoshi.co.jp`,  //変更
+    //     sitemap: `https://yanagawa-yoshi.co.jp/sitemap-index.xml`, //変更
+    //     policy: [{ userAgent: `*`, allow: `/` }]
+    //   }
+    // },
+    // {
+    //   resolve: `gatsby-plugin-canonical-urls`,
+    //   options: {
+    //     siteUrl: `https://yanagawa-yoshi.co.jp`, //変更
+    //     stripQueryString: true,
+    //   },
+    // },
     {
       resolve: 'gatsby-source-microcms',
       options: {
@@ -52,13 +59,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-plugin-canonical-urls`,
-      options: {
-        siteUrl: `https://yanagawa-yoshi.co.jp`, //変更
-        stripQueryString: true,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -73,23 +73,23 @@ module.exports = {
         icon: `src/images/logo_short.svg`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        trackingIds: [
-          `G-TN0FTC1C9G`,
-        ],
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        pluginConfig: {
-          head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-        },
-      },
-    }
+    // {
+    //   resolve: `gatsby-plugin-google-gtag`,
+    //   options: {
+    //     trackingIds: [
+    //       `G-TN0FTC1C9G`,
+    //     ],
+    //     gtagConfig: {
+    //       anonymize_ip: true,
+    //       cookie_expires: 0,
+    //     },
+    //     pluginConfig: {
+    //       head: true,
+    //       // Setting this parameter is also optional
+    //       respectDNT: true,
+    //     },
+    //   },
+    // }
     
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
